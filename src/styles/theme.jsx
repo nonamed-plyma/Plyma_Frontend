f18559b5de477486b7d914ef7fa4462527ee178e
seeum0507 <kwonstella0507@gmail.com>
2024-10-13 22:09:43 +0900

    사진 추가

--- src/styles/theme.jsx
+++ src/styles/theme.jsx
@@ -1,7 +1,15 @@
 export const color = {
   White: "#FFFFFF",
   Black: "#000000",
-  Blue: ["#52BAFF", "#2AAAFF", "#008DEC", "#0099FF", "#0673BC"], //0번은 400, 1번은 500, 2번은 600, 3번은 아주레
+  Blue: [
+    "#52BAFF",
+    "#2AAAFF",
+    "#008DEC",
+    "#0099FF",
+    "#0673BC",
+    "#97befd",
+    "#b4d7f5",
+  ], //0번은 400, 1번은 500, 2번은 600, 3번은 아주레
   Red: "#FF0000",
   Gray: [
     "#FAFAFA",
