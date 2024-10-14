aad9562dc7a0492a7a2c8269e73aaed573d49675
seeum0507 <kwonstella0507@gmail.com>
2024-10-14 23:22:42 +0900

    불필요한 이미지 삭제

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import BeforeMainPage from "./pages/Main/LoginBefore/beforeMain";
 import AfterHeader from "./components/Header/BeforeHeader/beforeHeader";
 import AfterMainPage from "./pages/Main/LoginAfter/afterMain";
+import StudentLogin from "./pages/Login/Student/studentLogin";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/beforemain" element={<BeforeMainPage />} />
         <Route path="/aftermain" element={<AfterMainPage />} />
+        <Route path="/studentLogin" element={<StudentLogin />} />
       </Routes>
     </>
   );
