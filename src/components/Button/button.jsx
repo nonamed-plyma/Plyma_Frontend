f53d1fab2337a061cea262c233435e49fb06c266
seeum0507 <kwonstella0507@gmail.com>
2024-10-16 22:41:12 +0900

    ♻️ :: button props 수정

--- src/components/Button/button.jsx
+++ src/components/Button/button.jsx
@@ -1,15 +1,11 @@
 import React from "react";
 import styled from "styled-components";
-<<<<<<< Updated upstream
-=======
-<<<<<<< Updated upstream
-=======
->>>>>>> Stashed changes
 import { color } from "../../styles/theme";
 
 const StyledButton = styled.button`
   width: ${(props) => props.width || "476px"};
   height: ${(props) => props.height || "60px"};
+<<<<<<< Updated upstream
   border-radius: ${(props) => props.borderRadius || "8px"};
   margin: ${(props) => props.margin || "0"};
   background-color: ${(props) => props.backgroundColor || "#008DEC"};
@@ -19,34 +15,8 @@ const StyledButton = styled.button`
   color: white;
 `;
 
-<<<<<<< Updated upstream
-const Button = ({
-  width,
-  height,
-  borderRadius,
-  backgroundColor,
-  children,
-  ...rest
-}) => {
-  return (
-    <StyledButton
-      width={width}
-      height={height}
-      borderRadius={borderRadius}
-      backgroundColor={backgroundColor}
-      {...rest}
-    >
-      {children}
-    </StyledButton>
-  );
-};
-
-export default Button;
-=======
 const Button = (props) => {
   return <StyledButton {...props} />;
 };
 
 export default Button;
->>>>>>> Stashed changes
->>>>>>> Stashed changes
