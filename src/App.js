e0278f820baae411739597f21a4182b7f469ac5e
seeum0507 <kwonstella0507@gmail.com>
2024-10-20 19:51:10 +0900

    Route 추가

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import BeforeMainPage from "./pages/Main/LoginBefore/beforeMain";
 import AfterMainPage from "./pages/Main/LoginAfter/afterMain";
+import StudentSignin from "./pages/Signin/Student/studentSignin";
+import CorrectSignin from "./pages/Signin/Student/CorrectSignin";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Routes>
         <Route path="/beforemain" element={<BeforeMainPage />} />
         <Route path="/aftermain" element={<AfterMainPage />} />
+        <Route path="/studentSignin" element={<StudentSignin />} />
+        <Route path="/correctSignin" element={<CorrectSignin />} />
       </Routes>
     </>
   );
