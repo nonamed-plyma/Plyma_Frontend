9e331031f7b7dfa237c536fc415f01ffd1c5fd88
seeum0507 <kwonstella0507@gmail.com>
2024-10-20 13:22:35 +0900

    버튼 컴포넌트 코드 수정

---
+++ src/components/Button/button.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components";
+import { color } from "../../styles/theme";
+
+const StyledButton = styled.button`
+  width: ${(props) => props.width || "476px"};
+  height: ${(props) => props.height || "60px"};
+  border-radius: ${(props) => props.borderRadius || "8px"};
+  margin: ${(props) => props.margin || "0"};
+  background-color: ${(props) => props.backgroundColor || "#008DEC"};
+  color: white;
+`;
+
+const Button = (props) => {
+  return <StyledButton {...props} />;
+};
+
+export default Button;
