94ee2b6365d5776d00f3ace9c3e9a4a7e98b5b16
seeum0507 <kwonstella0507@gmail.com>
2024-10-20 16:52:02 +0900

    학생 로그인 퍼블리싱

---
+++ src/pages/Login/Student/studentLogin.jsx
@@ -0,0 +1,107 @@
+import styled from "styled-components";
+import BeforeMainPage from "../../Main/LoginBefore/beforeMain"; // BeforeMain을 import
+import { color } from "../../../styles/theme"; // color import
+import Input from "../../../components/Input/input";
+import Div from "../../../components/Div/div";
+import Button from "../../../components/Button/button";
+import LogoImg from "../../../assets/img/logo.png";
+import xS from "../../../assets/svg/xImg(엑스표시).svg";
+
+export const Layer = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5); /* 어두운 배경 및 투명도 */
+  z-index: 1; /* 배경 위에 위치 */
+`;
+
+export const ModalDiv = styled(Div)`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 2;
+  background-color: ${color.White};
+  padding: 20px;
+  border-radius: 10px;
+  width: 550px;
+  height: 600px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start; /* 중앙 정렬 */
+`;
+
+export const Logo = styled.img`
+  width: 60px;
+  height: 45px;
+  margin-top: 5px; /* ModalDiv 기준으로 44px 아래 */
+`;
+
+export const IdInput = styled(Input)`
+  margin-top: -10px;
+  width: 350px;
+  height: 45px;
+`;
+
+export const PasswordInput = styled(Input)`
+  margin-top: 40px; /* IdInput 기준으로 48px 아래 */
+  width: 350px;
+  height: 45px;
+`;
+
+export const LoginButton = styled(Button)`
+  margin-top: 60px;
+  border: none;
+  font-size: 17px;
+`;
+
+export const NoAccount = styled.p`
+  font-weight: 500;
+  font-size: 17px;
+  margin-top: 30px;
+`;
+
+export const Link = styled.a`
+  color: ${color.Blue[2]};
+  font-weight: 600;
+  font-size: 17px;
+`;
+
+export const Title = styled.p`
+  font-size: 33px;
+  font-weight: 500;
+  margin-left: -340px; /* Title을 왼쪽으로 200px 이동 */
+  margin-top: 35px;
+`;
+export const XS = styled.img`
+  margin-left: 500px;
+`;
+
+function StudentLogin() {
+  return (
+    <>
+      <BeforeMainPage />
+      <Layer />
+      {/* 그 위에 로그인 레이어 추가 */}
+      <ModalDiv>
+        <XS src={xS}></XS>
+        <Logo src={LogoImg}></Logo>
+        <Title>로그인</Title>
+        <IdInput placeholder="아이디를 입력해주세요(4~10자)" />
+        <PasswordInput
+          placeholder="비밀번호를 입력해주세요(8~16자)"
+          type="password"
+        />
+        <LoginButton>로그인</LoginButton>
+        <NoAccount>
+          계정이 없으신가요? <Link>회원가입</Link>
+        </NoAccount>
+      </ModalDiv>
+    </>
+  );
+}
+
+export default StudentLogin;
