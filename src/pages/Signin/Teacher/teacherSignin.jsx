3c8811413687ffcb1bd4f9d82e62d72a3a337825
seeum0507 <kwonstella0507@gmail.com>
2024-10-20 19:39:25 +0900

    선생님 회원가입(teacherSignin) 퍼블리싱

---
+++ src/pages/Signin/Teacher/teacherSignin.jsx
@@ -0,0 +1,112 @@
+import styled from "styled-components";
+import BeforeMainPage from "../../Main/LoginBefore/beforeMain";
+import { color } from "../../../styles/theme";
+import Input from "../../../components/Input/input";
+import Div from "../../../components/Div/div";
+import Button from "../../../components/Button/button";
+import LogoImg from "../../../assets/img/logo.png";
+import xS from "../../../assets/svg/xImg(엑스표시).svg";
+
+export const Layer = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 1;
+`;
+
+export const ModalDiv = styled(Div)`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 2;
+  background-color: ${color.White};
+  padding: 20px;
+  border-radius: 10px;
+  width: 550px;
+  height: 600px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+`;
+
+export const Logo = styled.img`
+  width: 60px;
+  height: 45px;
+  margin-top: 5px;
+`;
+
+export const IdInput = styled(Input)`
+  margin-top: 30px;
+  width: 350px;
+  height: 45px;
+`;
+
+export const PasswordInput = styled(Input)`
+  margin-top: 30px;
+  width: 350px;
+  height: 45px;
+`;
+
+export const LoginButton = styled(Button)`
+  margin-top: 40px;
+  border: none;
+  font-size: 17px;
+`;
+
+export const NoAccount = styled.p`
+  font-weight: 500;
+  font-size: 17px;
+  margin-top: 25px;
+`;
+
+export const Link = styled.a`
+  color: ${color.Blue[2]};
+  font-weight: 600;
+  font-size: 17px;
+`;
+
+export const Title = styled.p`
+  font-size: 33px;
+  font-weight: 500;
+  margin-left: -340px;
+  margin-top: 15px;
+`;
+export const XS = styled.img`
+  margin-left: 500px;
+`;
+export const NameInput = styled(Input)`
+  margin-top: -5px;
+  width: 350px;
+  height: 45px;
+`;
+
+function TeacherSignin() {
+  return (
+    <>
+      <BeforeMainPage />
+      <Layer />
+      <ModalDiv>
+        <XS src={xS}></XS>
+        <Logo src={LogoImg}></Logo>
+        <Title>회원가입</Title>
+        <NameInput placeholder="이름을 입력해주세요(1~4자)"></NameInput>
+        <IdInput placeholder="아이디를 입력해주세요(4~10자)" />
+        <PasswordInput
+          placeholder="비밀번호를 입력해주세요(8~16자)"
+          type="password"
+        />
+        <LoginButton>회원가입</LoginButton>
+        <NoAccount>
+          계정이 있으신가요? <Link>로그인</Link>
+        </NoAccount>
+      </ModalDiv>
+    </>
+  );
+}
+
+export default TeacherSignin;
