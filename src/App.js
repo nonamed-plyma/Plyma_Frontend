eab0f1b9009efd936eed7b176206b8f9d0de4205
seeum0507 <kwonstella0507@gmail.com>
2024-10-20 20:21:08 +0900

    Route 추가

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import BeforeMainPage from "./pages/Main/LoginBefore/beforeMain";
 import AfterMainPage from "./pages/Main/LoginAfter/afterMain";
+import OnWritet from "./pages/MyPage/Teacher/OnWritet/onWritet";
+import OnGoodt from "./pages/MyPage/Teacher/OnGoodt/ondGoodt";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Routes>
         <Route path="/beforemain" element={<BeforeMainPage />} />
         <Route path="/aftermain" element={<AfterMainPage />} />
+        <Route path="/onWritet" element={<OnWritet />} />
+        <Route path="/onGoodt" element={<OnGoodt />} />
       </Routes>
     </>
   );
