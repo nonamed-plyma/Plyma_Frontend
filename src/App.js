17a4c28660b6cf9c14964d76275e55baf7dcf4ee
seeum0507 <kwonstella0507@gmail.com>
2024-10-21 23:11:54 +0900

    Route 추가

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import BeforeMainPage from "./pages/Main/LoginBefore/beforeMain";
 import AfterHeader from "./components/Header/BeforeHeader/beforeHeader";
 import AfterMainPage from "./pages/Main/LoginAfter/afterMain";
 import StudentLogin from "./pages/Login/Student/studentLogin";
+import DeleteOnList from "./pages/Delete/DeleteOnList/deleteOnList";
+import DeleteStudentMy from "./pages/Delete/DeleteOnMyPage/Student/deleteStudentMy";
+import DeleteTeacherMy from "./pages/Delete/DeleteOnMyPage/Teacher/deleteTeacherMy";
 
 function App() {
   return (
@@ -12,6 +15,9 @@ function App() {
         <Route path="/beforemain" element={<BeforeMainPage />} />
         <Route path="/aftermain" element={<AfterMainPage />} />
         <Route path="/studentLogin" element={<StudentLogin />} />
+        <Route path="/deleteList" element={<DeleteOnList />} />
+        <Route path="/deleteMyS" element={<DeleteStudentMy />} />
+        <Route path="/deleteMyT" element={<DeleteTeacherMy />} />
       </Routes>
     </>
   );
