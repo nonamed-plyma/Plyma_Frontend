94eb79fd536454b47e0ab50d93508c78853a4b18
seeum0507 <kwonstella0507@gmail.com>
2024-10-21 22:03:22 +0900

    Route추가

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import BeforeMainPage from "./pages/Main/LoginBefore/beforeMain";
 import AfterMainPage from "./pages/Main/LoginAfter/afterMain";
+import ListWatch from "./pages/Lists/ListWatch/listWatch";
+import ListWrite from "./pages/Lists/ListWrite/listWrite";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Routes>
         <Route path="/beforemain" element={<BeforeMainPage />} />
         <Route path="/aftermain" element={<AfterMainPage />} />
+        <Route path="/listWatch" element={<ListWatch />} />
+        <Route path="/listWrite" element={<ListWrite />} />
       </Routes>
     </>
   );
