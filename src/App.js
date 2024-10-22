f75a082d5cd6d6fa969817d465e6675daf7259d8
seeum0507 <kwonstella0507@gmail.com>
2024-10-22 16:52:38 +0900

    Route 경로 수정

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import "./App.css";
 import BeforeMainPage from "./pages/Main/LoginBefore/beforeMain";
 import AfterHeader from "./components/Header/BeforeHeader/beforeHeader";
 import AfterMainPage from "./pages/Main/LoginAfter/afterMain";
-import StudentLogin from "./pages/Login/Student/studentLogin";
 import DeleteOnList from "./pages/Delete/DeleteOnList/deleteOnList";
 import DeleteStudentMy from "./pages/Delete/DeleteOnMyPage/Student/deleteStudentMy";
 import DeleteTeacherMy from "./pages/Delete/DeleteOnMyPage/Teacher/deleteTeacherMy";
@@ -14,7 +13,6 @@ function App() {
       <Routes>
         <Route path="/beforemain" element={<BeforeMainPage />} />
         <Route path="/aftermain" element={<AfterMainPage />} />
-        <Route path="/studentLogin" element={<StudentLogin />} />
         <Route path="/deleteList" element={<DeleteOnList />} />
         <Route path="/deleteMyS" element={<DeleteStudentMy />} />
         <Route path="/deleteMyT" element={<DeleteTeacherMy />} />
