26d0f75f99f125414bc91d7c971effaf2484a29e
seeum0507 <kwonstella0507@gmail.com>
2024-10-22 14:33:02 +0900

    input 컴포넌트

--- src/components/Input/input.jsx
+++ src/components/Input/input.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import styled from "styled-components";
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
 const StyledInput = styled.input`
   width: ${(props) => props.width || "476px"};
   height: ${(props) => props.height || "60px"};
   border-radius: ${(props) => props.borderRadius || "8px"};
   border: 1px solid;
+<<<<<<< Updated upstream
   border-color: ${(props) => props.borderColor || "rgba(0, 0, 0, 0.52)"};
+=======
+  border-color: ${(props) => props.borderColor || "rgba(255, 0, 0, .52)"};
+>>>>>>> Stashed changes
   background-image: ${(props) => props.backgroundImage || "none"};
   background-repeat: no-repeat;
   background-size: 30px 25px, 24px 24px;
@@ -15,7 +23,15 @@ const StyledInput = styled.input`
   font-size: 16px;
   outline: none;
 `;
+<<<<<<< Updated upstream
 const Input = (props) => {
   return <StyledInput {...props} />;
 };
+=======
+
+const Input = (props) => {
+  return <StyledInput {...props} />;
+};
+
+>>>>>>> Stashed changes
 export default Input;
