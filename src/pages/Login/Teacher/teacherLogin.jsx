9695dd032a9d8ad3da6293495e144e96698fb938
seeum0507 <kwonstella0507@gmail.com>
2024-10-22 18:52:07 +0900

    비번 눈 이미지 추가

--- src/pages/Login/Teacher/teacherLogin.jsx
+++ src/pages/Login/Teacher/teacherLogin.jsx
@@ -6,6 +6,7 @@ import Div from "../../../components/Div/div";
 import Button from "../../../components/Button/button";
 import LogoImg from "../../../assets/img/logo.png";
 import xS from "../../../assets/svg/xImg(엑스표시).svg";
+import EyeOff from "../../../assets/img/inputEyeOffImg(눈꺼짐).png";
 
 export const Layer = styled.div`
   position: fixed;
@@ -50,6 +51,8 @@ export const PasswordInput = styled(Input)`
   margin-top: 40px;
   width: 350px;
   height: 45px;
+  background-size: 22px 22px;
+  background-position-x: 420px;
 `;
 
 export const LoginButton = styled(Button)`
@@ -93,6 +96,7 @@ function TeacherLogin() {
         <PasswordInput
           placeholder="비밀번호를 입력해주세요(8~16자)"
           type="password"
+          backgroundImage={`url(${EyeOff})`}
         />
         <LoginButton>로그인</LoginButton>
         <NoAccount>
